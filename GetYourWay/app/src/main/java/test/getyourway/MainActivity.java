7396275200541cde9f6951fa0140dbf01bc036fb
Sénthène <seycha.sth@live.fr>
2016-12-15 20:42:24 +0100

    Mis à jour du menu  (icônes à ajouter)

--- GetYourWay/app/src/main/java/test/getyourway/MainActivity.java
+++ GetYourWay/app/src/main/java/test/getyourway/MainActivity.java
@@ -82,17 +82,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_reception) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_ascenseur) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_toilettes) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_exit) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_changerEtage) {
 
         }
 
